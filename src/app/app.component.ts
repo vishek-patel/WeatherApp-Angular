115343e7b0d8b62b773107104aab6efcd7b0fc74
vishek-patel <vishek.12001921@lpu.in>
2023-03-23 10:45:29 +0530

    added all the files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {HttpClientJsonpModule} from '@angular/common/http'
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,67 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'weatherApp';
+  city:string = ""
+  API_KEY:any = "caaa0681fae72d1818249bc9511fabc8"
+  setTrue = false
+  // source = "winter.jpeg"
+  source ='../assets/default.jpeg'
+  cityName:string = ""
+  wind : any
+  humidity:any
+  pressure:any
+  longitude:any
+  latitude:any
+  country:any
+
+  temp :any = 0
+
+  placeValue :string = "Enter a city name..."
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  getWeather() {
+    this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.API_KEY}`).subscribe((data:any) =>{
+  // this.http.jsonp(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.API_KEY}`, 'callback').subscribe((data:any) =>{
+    this.temp = data.main.temp;
+    this.wind = data.wind.speed;
+    this.humidity = data.main.humidity;
+    this.pressure = data.main.pressure;
+    this.longitude = data.coord.lon;
+    this.latitude = data.coord.lat;
+    this.country = data.sys.country;
+    this.cityName = data.name;
+    this.temp  -= 273.15
+    this.temp = this.temp.toFixed(2)
+
+    if(this.temp > 20 ){
+      this.source = '../assets/summer.jpeg'
+    }
+    else if(this.temp > 15 && this.temp < 20){
+      this.source = '../assets/spring.jpeg'
+    }
+    else  {
+      this.source = '../assets/winter.jpeg'
+    }
+    this.longitude = this.longitude.toFixed(2)
+    this.latitude = this.latitude.toFixed(2)
+    // this.wind = this.wind.toFixed(2)
+    this.wind = this.wind * 60 * 60 / 1000
+    this.wind = this.wind.toFixed(2)
+// prints 3.6
+
+   console.log(data)
+  })
+  
+  setTimeout(() => {
+  // console.log(this.temp.main.temp)
+ 
+
+  this.setTrue = true
+  },1000)
+
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClientJsonpModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
